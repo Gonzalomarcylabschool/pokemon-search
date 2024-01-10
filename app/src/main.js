e3ae0c3dc7bde0c91834a645c56cf981ef356f00
gonzalorportfolio <gromeroeducation@gmail.com>
2024-01-10 16:02:11 -0500

    update

--- app/src/main.js
+++ app/src/main.js
@@ -1,9 +1,17 @@
 import './style.css'
+import { renderOnePokemon } from './utils/render.js';
 
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target)
+  const dataObj = Object.fromEntries(formData);
+  // const data = Object.fromEntries(new FormData(e.target));
+  console.log(dataObj.pokemonName);
+  renderOnePokemon(dataObj.pokemonName);
+}
 const main = () => {
   console.log('working');
-
-
+  document.querySelector('form').addEventListener('submit', handleSubmit);
 } 
 
 main();
